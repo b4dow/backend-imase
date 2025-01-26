7be889cb2b3db55f328e91a4e9bca14fbbb331e0
Luidev <alfonso765123@gmail.com>
2025-01-26 16:23:29 +0100

    add corsOptions

--- config/corsOptions.js
+++ config/corsOptions.js
@@ -1,18 +1,20 @@
 const { frontendUrl } = require('./config');
 
 const corsOptions = {
- origin: (origin, callback) => {
-  const whiteList = ['https://localhost:3000'];
-  console.log(process.argv);
-  if (process.argv[2] === '--api') {
-   whiteList.push(undefined);
-  }
-  if (whiteList.includes(origin)) {
-   callback(null, true);
-  } else {
-   callback(new Error('Not allowed by CORS'));
-  }
- },
+  origin: (origin, callback) => {
+    const whiteList = [frontendUrl];
+    console.log(whiteList.includes(origin))
+    console.log('estoy aqui')
+    console.log(origin)
+    if (process.argv[2] === '--api') {
+      whiteList.push(undefined);
+    }
+    if (whiteList.includes(origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
 };
 
 module.exports = { corsOptions };
--- index.js
+++ index.js
@@ -5,25 +5,18 @@ const cors = require('cors');
 const cookieParser = require('cookie-parser');
 const routerApi = require('./routes');
 const {
- logErrors,
- errorHandlers,
- boomErrorHandler,
- ormErrorHandler,
+  logErrors,
+  errorHandlers,
+  boomErrorHandler,
+  ormErrorHandler,
 } = require('./middleware/error.handler');
-const { frontendUrl } = require('./config/config');
 const { corsOptions } = require('./config/corsOptions');
-// const { corsOptions } = require('./config/corsOptions');
 
 const app = express();
 const PORT = 4000;
 
 // Middlewares
-app.use(
- cors({
-  origin: frontendUrl,
-  credentials: true,
- })
-);
+app.use(cors(corsOptions));
 
 app.use(express.json({ limit: '10mb' }));
 app.use(morgan('dev'));
@@ -40,5 +33,5 @@ app.use(ormErrorHandler);
 app.use(errorHandlers);
 
 app.listen(PORT, () => {
- console.log(colors.magenta.bold(`Listening on port ${PORT}`));
+  console.log(colors.magenta.bold(`Listening on port ${PORT}`));
 });
